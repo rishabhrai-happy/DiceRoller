5b5400e6022d1cb62823382f26b20e2da6b66477
Hp <rishabhrai980@gmail.com>
2020-09-04 14:25:51 +0530

    use val resultText for store the result

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice(){
         Toast.makeText(this,"button clicked",Toast.LENGTH_SHORT).show()
         val randomInt =(1 ..6).random()
+        val resultText:TextView=findViewById(R.id.result_text)
 
 
     }
